e93a3fcbcf590fc9fede4c8cadb2ca9ee109ec69
Androyen <androyen@aol.com>
2014-10-13 22:15:54 -0500

    Set up AlertDialog in SignUp button to check for input errors

--- app/src/main/java/com/androyen/ribbit/SignUpActivity.java
+++ app/src/main/java/com/androyen/ribbit/SignUpActivity.java
@@ -1,17 +1,62 @@
 package com.androyen.ribbit;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class SignUpActivity extends Activity {
+
+    protected EditText mUsername;
+    protected EditText mPassword;
+    protected EditText mEmail;
+    protected Button mSignUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mUsername = (EditText) findViewById(R.id.userNameField);
+        mPassword = (EditText) findViewById(R.id.passwordField);
+        mEmail = (EditText) findViewById(R.id.emailField);
+        mSignUpButton = (Button) findViewById(R.id.signupButton);
+
+        mSignUpButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                //Get string. It is in Editable form. Convert to string
+                String username = mUsername.getText().toString();
+                String password = mPassword.getText().toString();
+                String email = mEmail.getText().toString();
+
+                //Trim white space from input in EditText
+                username = username.trim();
+                password = password.trim();
+                email = email.trim();
+
+                //Check if input is empty blank
+                if (username.isEmpty() || password.isEmpty() || email.isEmpty()) {
+                       AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+                       builder.setMessage(R.string.signup_error_message);
+                       builder.setTitle(R.string.signup_error_title);
+                    //Parameter null lets dialog be dismissed
+                       builder.setPositiveButton(android.R.string.ok, null);
+                       AlertDialog dialog = builder.create();
+                }
+                else {
+                    //Create new user
+                }
+
+            }
+        });
     }
 
 
